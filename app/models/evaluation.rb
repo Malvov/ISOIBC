2c6e41b61ce5c9b848f79b6188fde6f773e8fa73
malvov <manuel.olivaresbonilla@gmail.com>
2018-03-21 08:34:26 -0600

    deleted test suite because I'm not using it

--- app/models/evaluation.rb
+++ app/models/evaluation.rb
@@ -14,6 +14,7 @@
 #
 
 class Evaluation < ApplicationRecord
+
   belongs_to :employee
   belongs_to :task
   
--- db/structure.sql
+++ db/structure.sql
@@ -106,38 +106,6 @@ ALTER SEQUENCE evaluations_id_seq OWNED BY evaluations.id;
 
 
 --
--- Name: parameters; Type: TABLE; Schema: public; Owner: -
---
-
-CREATE TABLE parameters (
-    id bigint NOT NULL,
-    name character varying,
-    value integer,
-    created_at timestamp without time zone NOT NULL,
-    updated_at timestamp without time zone NOT NULL
-);
-
-
---
--- Name: parameters_id_seq; Type: SEQUENCE; Schema: public; Owner: -
---
-
-CREATE SEQUENCE parameters_id_seq
-    START WITH 1
-    INCREMENT BY 1
-    NO MINVALUE
-    NO MAXVALUE
-    CACHE 1;
-
-
---
--- Name: parameters_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: -
---
-
-ALTER SEQUENCE parameters_id_seq OWNED BY parameters.id;
-
-
---
 -- Name: schema_migrations; Type: TABLE; Schema: public; Owner: -
 --
 
@@ -270,13 +238,6 @@ ALTER TABLE ONLY evaluations ALTER COLUMN id SET DEFAULT nextval('evaluations_id
 -- Name: id; Type: DEFAULT; Schema: public; Owner: -
 --
 
-ALTER TABLE ONLY parameters ALTER COLUMN id SET DEFAULT nextval('parameters_id_seq'::regclass);
-
-
---
--- Name: id; Type: DEFAULT; Schema: public; Owner: -
---
-
 ALTER TABLE ONLY tasks ALTER COLUMN id SET DEFAULT nextval('tasks_id_seq'::regclass);
 
 
@@ -316,14 +277,6 @@ ALTER TABLE ONLY employees
 
 ALTER TABLE ONLY evaluations
     ADD CONSTRAINT evaluations_pkey PRIMARY KEY (id);
-
-
---
--- Name: parameters_pkey; Type: CONSTRAINT; Schema: public; Owner: -
---
-
-ALTER TABLE ONLY parameters
-    ADD CONSTRAINT parameters_pkey PRIMARY KEY (id);
 
 
 --
@@ -448,7 +401,6 @@ INSERT INTO "schema_migrations" (version) VALUES
 ('20180312143933'),
 ('20180312163057'),
 ('20180315213425'),
-('20180320153956'),
-('20180320165953');
+('20180320153956');
 
 
