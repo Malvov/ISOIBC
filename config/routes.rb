93dd091593a2b67f383748cf5a872d394c1ad889
malvov <manuel.olivaresbonilla@gmail.com>
2018-06-05 09:04:18 -0600

    some minor changes

--- config/routes.rb
+++ config/routes.rb
@@ -15,7 +15,7 @@ Rails.application.routes.draw do
   devise_for :users
   root 'static_pages#home'
 
-  resources :evaluations 
+  resources :evaluations
   resources :employees
   resources :person_responsibles
   resources :zones
